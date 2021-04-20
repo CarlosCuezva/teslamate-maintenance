b1f02890255614348b3d9859441fa622a3f2dd19
Carlos Cuezva <carlos@cuezva.net>
2021-04-20 11:55:07 +0200

    El backup ahora se hace desde el propio script de backup

--- updateTeslaMate.sh
+++ updateTeslaMate.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 #
-# Description: 
+# Description: Script para actualizar TeslaMate
 #
 # Author: Carlos Cuezva
 # Created: 18/04/2021
 
+PATH_SCRIPT=`pwd`
+PATH_COMPOSE=$HOME
+PATH_BACKUP=$HOME/TeslaMateBackup
+
 echo '¿Deseas hacer una copia de seguridad de la base de datos (s/n)? '
 read answer
 
+cd $PATH_COMPOSE
 if [ "$answer" != "${answer#[Ss]}" ] ;then
-    filename=`date +"%Y%m%d"`_teslamate.bck
-    docker-compose exec -T database pg_dump -U teslamate teslamate > ./$filename
+    $PATH_SCRIPT/backupTeslaMate.sh
 fi
 
-echo '\n---- Comenzando la actualización ----\n'
+echo -e '\n---- Comenzando la actualización ----\n'
 docker-compose pull
-echo '\n---- Finalizada la actualización ----\n'
+echo -e '\n---- Finalizada la actualización ----\n'
 
-echo '\n---- Reiniciando docker de TeslaMate ----\n'
+echo -e '\n---- Reiniciando docker ----\n'
 docker-compose up -d
+echo -e '\n---- Reinicio completado ----\n'
