a89e5d2cda25269bf0f1d54b3d890f0ecd36c11b
Carlos Cuezva Ticio <5815421+carloscuezva@users.noreply.github.com>
2023-04-07 08:51:17 +0200

    Changed the date format of the backup file

--- backupTeslaMate.sh
+++ backupTeslaMate.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 #
-# Description: Script para hacer backup de la base de datos de TeslaMate
+# Description: Script to backup the TeslaMate database
 #
 # Author: Carlos Cuezva
 # Created: 23/04/2021
-# Last update: 22/09/2021
+# Last update: 07/04/2023
 
 PATH_COMPOSE=$HOME
-PATH_BACKUP=$HOME/TeslaMateBackup
+PATH_BACKUP=$HOME/backups/teslamate
+REMOVE_OLDER=7
 
-echo -e '\n---- Comenzando el backup ----\n'
+echo -e '\n---- Starting the backup ----\n'
 
 cd $PATH_COMPOSE
-FILENAME=`date +"%Y%m%d"`_teslamate.bck
-docker-compose exec -T database pg_dump -U teslamate teslamate > $PATH_BACKUP/$FILENAME
+FILENAME=`date +"%Y%m%d_%H%M"`_teslamate.bck
+docker exec postgres pg_dump -U teslamate teslamate > $PATH_BACKUP/$FILENAME
 
 cd $PATH_BACKUP
 gzip -9 -f $FILENAME
 
 FILESIZE=`du -h "$FILENAME.gz" | cut -f1`
-echo -e "\nLa copia de seguridad tiene un tamaño de $FILESIZE\n"
+echo -e "\nThe size of the backup is $FILESIZE\n"
 
-echo -e "\nQuitando copias de seguridad con más de 15 días de antigüedad\n"
-find $PATH_BACKUP -type f -name '*_teslamate.bck.gz' -mtime +15 | xargs rm
+echo -e "\nRemoving backups older than $REMOVE_OLDER days\n"
+find $PATH_BACKUP -name '*_teslamate.bck.gz' -type f -mtime +$REMOVE_OLDER -delete
 
-echo -e '\n---- Finalizado el backup ----\n'
+echo -e '\n---- Backup finished ----\n'
